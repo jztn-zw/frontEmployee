34d1a10a171fb40ab6f45bc4ee806769111dfdc9
Jaztin <jaztincontreras2805@gmail.com>
2026-02-26 10:59:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const content=document.querySelector('#content');
+
+window.addEventListener('load', ()=>{
+    getUsers();
+});
+
+function getUsers(){
+    let html=""
+    fetch('https://bscs3-employee-3.onrender.com/api/users', {mode:'cors'})
+    .then(response=>{
+        console.log(response);
+        return response.json();
+    })
+    .then(data=>{
+        console.log(data);
+        data.forEach(element=>{
+            html+=`<li>${element.first_name}${element.last_name}<li>`;
+        })
+        content.innerHTML=html;
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+}
